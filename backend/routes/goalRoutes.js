6b7037c7398f674ffdd616f9b15b22bf2cfe4293
Momin-129 <momin.rather@gmail.com>
2023-06-26 11:41:08 +0530

    jwt authorization

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,11 +7,13 @@ const {
   deleteGoals,
 } = require("../controllers/goalController.js");
 
+const { protect } = require("../middleware/authMiddleware.js");
+
 // router.get("/", getGoals);
 // router.post("/", setGoals);
 // insted of above two lines can be done in one
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
-router.route("/:id").put(updateGoals).delete(deleteGoals);
+router.route("/:id").put(protect, updateGoals).delete(protect, deleteGoals);
 
 module.exports = router;
